7870f016409bcc40923700bda24f96c05a3c47ae
Michael Zwickler <zwickler.michael@googlemail.com>
2022-01-25 12:39:16 +0100

    sign_in_page userstory completed and passing initially

--- app.rb
+++ app.rb
@@ -5,12 +5,24 @@ require_relative 'lib/database_connection'
 DatabaseConnection.setup
 
 class Makersbnb < Sinatra::Base
+  
+  enable :sessions
+  
   get "/" do
     'Hello'
   end
 
+  get '/spaces' do
+    "Hi #{session[:username]}"
+  end
+
   get '/sessions/new' do
-    "Type in your name here" 
+    erb(:'sessions/new')
+  end
+
+  post '/sessions' do
+    session[:username] = params[:username]
+    redirect '/spaces'
   end
 
 end
