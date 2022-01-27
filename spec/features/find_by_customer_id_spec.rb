c2d08f65c3915028b245f7ff45773cbd2db998f9
Alex Sweeney <alexsweeney634@gmail.com>
2022-01-27 16:53:16 +0000

    owner id

--- spec/features/find_by_customer_id_spec.rb
+++ spec/features/find_by_customer_id_spec.rb
@@ -2,10 +2,10 @@ feature 'Find bookings by customer ID' do
   scenario 'Will be able to display any bookings currently held by a customer' do
 
     user = Users.create('justine@test.com', 'secrets', 'Justine')
-    Properties.create('House', 'Lovely House', 50, 2, '2022-02-01', '2022-02-28')
-    Properties.create('Cottage', 'Lovely Cottage', 50, 2, '2022-02-01', '2022-02-28')
+    Properties.create('House', 'Lovely House', 50, user.id, '2022-02-01', '2022-02-28')
+    Properties.create('Cottage', 'Lovely Cottage', 50, user.id, '2022-02-01', '2022-02-28')
     
-    booking = Booking.create('1', '154', '2022-02-02')
+    booking = Booking.create(properties.id, user.id, '2022-02-02')
 
     visit '/login'
 
