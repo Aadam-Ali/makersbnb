d8b4c0cdf6f7e4326888b4cf19fe060a9ecb49cc
Aadam Ali <ali.aadam@outlook.com>
2022-01-27 14:32:50 +0000

    implemented filter by bookings for owned properties

--- spec/unit/bookings_spec.rb
+++ spec/unit/bookings_spec.rb
@@ -49,4 +49,27 @@ describe Bookings do
       expect(results[2].property_id).to eq property_four.first['id']
     end
   end
+
+  describe '.find_incoming_bookings' do
+    it 'filters by incoming bookings' do
+      owner = DatabaseConnection.query("INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *;", ['test@test.com', 'password', 'Aadam'])
+      customer = DatabaseConnection.query("INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *;", ['alex@test.com', 'password', 'Alex'])
+
+      property_one = add_row_to_database('Small apartment', 'A small appartment in the city', 150, owner.first['id'], '2022-02-01', '2022-02-28')
+      property_two = add_row_to_database('Medium apartment', 'A medium appartment in the city', 300, owner.first['id'], '2022-02-01', '2022-02-28')
+      property_three = add_row_to_database('Large apartment', 'A large appartment in the city', 450, customer.first['id'], '2022-02-01', '2022-02-28')
+      property_four = add_row_to_database('Extra large apartment', 'An extra large appartment in the city', 600, owner.first['id'], '2022-02-01', '2022-02-28')
+
+      Bookings.create(property_one.first['id'], customer.first['id'], '2022-02-02')
+      Bookings.create(property_two.first['id'], customer.first['id'], '2022-02-02')
+      Bookings.create(property_three.first['id'], owner.first['id'], '2022-02-02')
+      Bookings.create(property_four.first['id'], customer.first['id'], '2022-02-02')
+
+      results = Bookings.find_incoming_bookings(owner.first['id'])
+
+      expect(results[0].property_id).to eq property_one.first['id']
+      expect(results[1].property_id).to eq property_two.first['id']
+      expect(results[2].property_id).to eq property_four.first['id']
+    end
+  end
 end
