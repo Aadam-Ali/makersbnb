95d458154e2a66c911554093b6f6fb585ca22673
Aadam Ali <ali.aadam@outlook.com>
2022-01-27 13:35:26 +0000

    bookings by customer

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -11,6 +11,7 @@ class Bookings
   end
 
   def self.create(property_id, customer_name, booking_date)
+    return false unless self.available?(property_id, booking_date)
     result = DatabaseConnection.query('INSERT INTO bookings (property_id, customer_id, booking_date) VALUES ($1, $2, $3) RETURNING *;',
     [property_id, customer_name, booking_date])
     Bookings.new(result[0]['id'], result[0]['property_id'], result[0]['customer_id'], result[0]['booking_date'], result[0]['status'])
@@ -19,5 +20,20 @@ class Bookings
   def self.find_by_customer_id(customer_id)
     results = DatabaseConnection.query('SELECT * FROM bookings WHERE customer_id = $1;', [customer_id])
     results.map { |booking| Bookings.new(booking['id'], booking['property_id'], booking['customer_id'], booking['booking_date'], booking['status']) }
+  end
+
+  private
+
+  def self.get_dates(property_id)
+    dates = DatabaseConnection.query("SELECT available_from, available_to FROM properties WHERE id = $1;", [property_id])[0]
+    {available_from: Date.parse(dates['available_from']),
+     available_to: Date.parse(dates['available_to'])}
+  end
+
+  def self.available?(property_id, date)
+    date = Date.parse(date)
+    valid_dates = self.get_dates(property_id)
+
+    date.between?(valid_dates[:available_from], valid_dates[:available_to]) ? true : false
   end
 end
--- spec/unit/bookings_spec.rb
+++ spec/unit/bookings_spec.rb
@@ -15,6 +15,16 @@ describe Bookings do
       expect(booking.booking_date).to eq '2022-02-02'
       expect(booking.status).to eq 'pending'
     end
+
+    it 'returns unsuccesful when entering a date that is outside the available range' do
+      owner = DatabaseConnection.query("INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *;", ['test@test.com', 'password', 'Aadam'])
+      customer = DatabaseConnection.query("INSERT INTO users (email, password, name) VALUES ($1, $2, $3) RETURNING *;", ['alex@test.com', 'password', 'Alex'])
+      property = add_row_to_database('Small apartment', 'A small appartment in the city', 150, owner.first['id'], '2022-02-01', '2022-02-28')
+
+      booking = Bookings.create(property.first['id'], customer.first['id'], '2022-03-02')
+
+      expect(booking).to eq false
+    end
   end
 
   describe '.find_by_customer_id' do
