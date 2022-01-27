fc8b65784d0753307daca3c7dc7d2c9d92b48c5e
Aadam Ali <ali.aadam@outlook.com>
2022-01-27 11:14:11 +0000

    implemented find bookings by customer id

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -15,4 +15,9 @@ class Bookings
     [property_id, customer_name, booking_date])
     Bookings.new(result[0]['id'], result[0]['property_id'], result[0]['customer_id'], result[0]['booking_date'], result[0]['status'])
   end
+
+  def self.find_by_customer_id(customer_id)
+    results = DatabaseConnection.query('SELECT * FROM bookings WHERE customer_id = $1;', [customer_id])
+    results.map { |booking| Bookings.new(booking['id'], booking['property_id'], booking['customer_id'], booking['booking_date'], booking['status']) }
+  end
 end
