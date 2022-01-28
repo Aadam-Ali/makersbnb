5979c6b038cb01af75224336273de94285f97dbb
Aadam Ali <ali.aadam@outlook.com>
2022-01-28 11:25:54 +0000

    fixed argument name

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -10,10 +10,10 @@ class Bookings
     @status = status
   end
 
-  def self.create(property_id, customer_name, booking_date)
+  def self.create(property_id, customer_id, booking_date)
     return false unless available?(property_id, booking_date)
     result = DatabaseConnection.query('INSERT INTO bookings (property_id, customer_id, booking_date) VALUES ($1, $2, $3) RETURNING *;',
-    [property_id, customer_name, booking_date])
+    [property_id, customer_id, booking_date])
     Bookings.new(result[0]['id'], result[0]['property_id'], result[0]['customer_id'], result[0]['booking_date'], result[0]['status'])
   end
 
